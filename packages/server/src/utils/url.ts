484795a2e17f6f09ed479f5c4352754e8bd0f4da
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-09-10 20:40:06 +0200

    url fix

--- packages/server/src/utils/url.ts
+++ packages/server/src/utils/url.ts
@@ -6,21 +6,31 @@ export function parseURLParams(url: string) {
   return search.split("&").reduce(
     (acc, pair) => {
       const [key, value] = pair.split("=");
-      acc[key] = value;
+      if (acc[key]) {
+        acc[key] = [acc[key], value].flat();
+        return acc;
+      } else {
+        acc[key] = value;
+      }
       return acc;
     },
-    {} as Record<string, string>
+    {} as Record<string, string | string[]>
   );
 }
 
 export function replaceURLParams(
   url: string,
-  params: Record<string, string | number>
+  params: Record<string, string | number | string[] | number[]>
 ): string {
   const existingParams = parseURLParams(url);
 
   const search = Object.entries({ ...existingParams, ...params })
-    .map(([key, value]) => `${key}=${value}`)
+    .map(([key, value]) => {
+      if (Array.isArray(value)) {
+        return value.map((v) => `${key}=${v}`).join("&");
+      }
+      return `${key}=${value}`;
+    })
     .join("&");
 
   return `${url.split("?")[0]}?${search}`;
