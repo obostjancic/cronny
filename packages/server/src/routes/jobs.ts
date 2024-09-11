70cd5b0caa1a80cc0163fec900ea61c081ec9e61
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-09-11 22:00:23 +0200

    expose post/patch job

--- packages/server/src/routes/jobs.ts
+++ packages/server/src/routes/jobs.ts
@@ -1,30 +1,42 @@
 import { Request, Response } from "express";
 
 import AsyncRouter from "./router.js";
-import { getJob, getJobs } from "../db/job.js";
+import { getJob, getJobs, saveJob, updateJob } from "../db/job.js";
 import { getJobRuns, getLastRun, getRun } from "../db/run.js";
+import { UnsavedJob } from "@cronny/types";
 
 const router = AsyncRouter();
 
 router.get("/", async (_: Request, res: Response) => {
   const jobs = await getJobs();
-  res.json(jobs);
+  return res.json(jobs);
 });
 
 router.get("/:id", async (req: Request, res: Response) => {
   const job = await getJob(+req.params.id);
   const runs = await getJobRuns(+req.params.id);
-  res.json({ ...job, runs });
+  return res.json({ ...job, runs });
+});
+
+router.post("/:id", async (req: Request, res: Response) => {
+  const body = req.body as UnsavedJob;
+  const job = await saveJob(body);
+  return res.json(job);
+});
+
+router.patch("/:id", async (req: Request, res: Response) => {
+  const job = await updateJob(+req.params.id, req.body);
+  return res.json(job);
 });
 
 router.get("/:id/runs/:runId", async (req: Request, res: Response) => {
   if (req.params.runId === "latest") {
     const run = await getLastRun(+req.params.id);
-    res.json(run);
+    return res.json(run);
     return;
   }
   const run = await getRun(+req.params.runId);
-  res.json(run);
+  return res.json(run);
 });
 
 export default router;
