5d5a6132e8f1c5a0a66430e95f48ab5efcd4b0cf
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-03-08 22:33:35 +0100

    path fix v3

--- packages/client/src/api/utils.tsx
+++ packages/client/src/api/utils.tsx
@@ -1,10 +1,8 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 export async function fetchJson(url: string, options: AxiosRequestConfig = {}) {
   try {
-    const response = await axios(`${API_URL}${url}`, {
+    const response = await axios(url, {
       headers: { "Content-Type": "application/json" },
       ...options,
     });
