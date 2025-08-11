42b093e2632128193ed296003948f6f13717954c
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-08-11 12:44:17 +0200

    build: fix

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'cronny',
+      script: 'packages/server/dist/index.js',
+      cwd: '/home/deployments/cronny',
+      env: {
+        NODE_ENV: 'production'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
