b1b0f98cbc7d1baa3c75049358804478f99c9e91
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-12-20 18:01:33 +0100

    fix: proxy url

--- packages/server/src/utils/request.ts
+++ packages/server/src/utils/request.ts
@@ -47,7 +47,7 @@ export async function fetchMultiplePages<T>(
 export async function fetchViaProxy(
   url: string,
   config?: Parameters<typeof axios.get>[1],
-  proxyUrl = "https://proxy.ognjen-bostjancic.workers.dev",
+  proxyUrl = "https://proxy.ogi.fyi",
 ) {
   return axios.get(`${proxyUrl}/?url=${encodeURIComponent(url)}`, config);
 }
